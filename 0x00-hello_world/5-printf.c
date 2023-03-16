25369931f66c17ef99d273576ad2e4a3193f90ba
omazoz <oumaymamazoz12@gmail.com>
2023-03-16 21:26:15 +0100

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,");
-    return (0);
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
