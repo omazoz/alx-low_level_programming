cdc6a75ff9946dcc7d71699b0e0b6f4b5fd7a18f
omazoz <oumaymamazoz12@gmail.com>
2023-03-16 22:04:47 +0100

    done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<unistd.h>
+#include <string.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+size_t len = strlen(str);
+fwrite(str, sizeof(char), len, stdout);
+return (0);
+}
