ae19f92b87015e44ee2f6a491d4b04552e1a5fd2
omazoz <oumaymamazoz12@gmail.com>
2023-03-17 23:46:50 +0100

    done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * lets print numbers
+ * 
+ * Return: 0 (SUCCESS)
+ */
+
+int main(void)
+{
+    puts("0123456789");
+    putchar('\n');
+    return(0);
+}
