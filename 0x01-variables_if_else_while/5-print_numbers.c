ed0571d562875f6b2e409d576d2b0f259dc70b24
omazoz <oumaymamazoz12@gmail.com>
2023-03-17 23:55:57 +0100

    done

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,12 +3,11 @@
 /**
  * main - Entry point
  * lets print numbers
- * 
  * Return: 0 (SUCCESS)
  */
 
 int main(void)
 {
-    puts("0123456789");
-    return(0);
+puts("0123456789");
+return (0);
 }
