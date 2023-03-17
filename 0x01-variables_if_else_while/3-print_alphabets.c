65eef858122f6feb98a38818f35d1510950b8124
omazoz <oumaymamazoz12@gmail.com>
2023-03-17 23:39:58 +0100

    done

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * lets play what I call the alphabet game again
+ * but  this time uppercase and lowercase
+ * Return: 0 (SUCCESS)
+ */
+
+int main(void)
+{
+char low;
+char app;
+
+low = 'a';
+app = 'A';
+while (low <= 'z')
+{
+putchar(low);
+low++;
+}
+while (b <= 'Z')
+{
+putchar(app);
+app++;
+}
+putchar('\n');
+return (0);
+}
