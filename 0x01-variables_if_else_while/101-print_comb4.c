95a999711a3f59e850ea8984a4697d6d38f89cd5
omazoz <oumaymamazoz12@gmail.com>
2023-03-18 01:29:09 +0100

    done

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,20 +6,20 @@
  */
 int main(void)
 {
-	int a, b, c;
+	int a, b, l;
 
 	for (a = 48; a < 58; a++)
 	{
-		for (m = 49; b < 58; b++)
+		for (b = 49; b < 58; b++)
 		{
-			for (c = 50; c < 58; l++)
+			for (c = 50; l < 58; c++)
 			{
 				if (c > b && b > a)
 				{
 					putchar(a);
 					putchar(b);
 					putchar(c);
-					if (a != 55 || b != 56)
+					if (a != 55 || b  != 56)
 					{
 						putchar(',');
 						putchar(' ');
