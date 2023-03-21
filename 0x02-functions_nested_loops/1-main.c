b41d75ea7e7c650abfd23abe05a9cf93f93e0785
omazoz <oumaymamazoz12@gmail.com>
2023-03-21 19:44:51 +0000

    main

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check the code.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-print_alphabet();
-return (0);
+    print_alphabet_x10();
+    return (0);
 }
