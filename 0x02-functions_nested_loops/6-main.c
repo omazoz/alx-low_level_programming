2e530c6f0dc5fa53098898e9adb0c0d9f7faaa09
omazoz <oumaymamazoz12@gmail.com>
2023-03-21 20:11:08 +0000

    done

---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = _abs(-1);
+    printf("%d\n", r);
+    r = _abs(0);
+    printf("%d\n", r);
+    r = _abs(1);
+    printf("%d\n", r);
+    r = _abs(-98);
+    printf("%d\n", r);
+    return (0);
+}
