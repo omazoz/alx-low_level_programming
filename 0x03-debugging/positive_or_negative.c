58ede1e0e030a5eaf5eff2a6aa658517ff5a0ca8
omazoz <oumaymamazoz12@gmail.com>
2023-03-23 00:44:06 +0000

    done

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,27 @@
+
+/* betty style doc for function main goes there */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int positive_or_negative( int n) 
+{
+
+	if (n > 0)
+	{
+	printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+	printf("%d is negative\n", n);
+	}
+	else
+	{
+	printf("%d is zero\n", n);
+	}
+	return (0);
+}
