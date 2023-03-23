530cba62fac7242d59084bf3e3995f7a2793fb7c
omazoz <oumaymamazoz12@gmail.com>
2023-03-23 01:31:59 +0000

    fixed

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,13 +11,13 @@ int i;
 
 printf("Infinite loop incoming :(\n");
 
-/*i = 0;*/
-
-while (i < 10)
-{
-putchar(i);
-}
-
+i = 0;
+/*
+*while (i < 10)
+*{
+*putchar(i);
+*}
+*/
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
