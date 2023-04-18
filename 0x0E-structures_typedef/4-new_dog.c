65c60e87b626ef233488b4b027fdfd214026cc69
omazoz <oumaymamazoz12@gmail.com>
2023-04-18 00:50:20 +0000

    fixed ?

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,32 +12,32 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *p_dog;
-	int i, lname, lowner;
+dog_t *p_dog;
+int i, lname, lowner;
 
-	p_dog = malloc(sizeof(*p_dog));
-	if (p_dog == NULL || !(name) || !(owner))
-	{
-		free(p_dog);
-		return (NULL);
-	}
+p_dog = malloc(sizeof(*p_dog));
+if (p_dog == NULL || !(name) || !(owner))
+{
+free(p_dog);
+return (NULL);
+}
 
-	for (lname = 0; name[lname]; lname++)
-		;
+for (lname = 0; name[lname]; lname++)
+;
 
-	for (lowner = 0; owner[lowner]; lowner++)
-		;
+for (lowner = 0; owner[lowner]; lowner++)
+;
 
-	p_dog->name = malloc(lname + 1);
-	p_dog->owner = malloc(lowner + 1);
+p_dog->name = malloc(lname + 1);
+p_dog->owner = malloc(lowner + 1);
 
-	if (!(p_dog->name) || !(p_dog->owner))
-	{
-		free(p_dog->owner);
-		free(p_dog->name);
-		free(p_dog);
-		return (NULL);
-	}
+if (!(p_dog->name) || !(p_dog->owner))
+{
+free(p_dog->owner);
+free(p_dog->name);
+free(p_dog);
+return (NULL);
+}
 
 	for (i = 0; i < lname; i++)
 		p_dog->name[i] = name[i];
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -9,10 +9,10 @@
  */
 void free_dog(dog_t *d)
 {
-	if (d)
-	{
-		free(d->name);
-		free(d->owner);
-		free(d);
-	}
+if (d)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
 }
