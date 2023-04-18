e9e1cd2db932be9486170aa06d5d6b15ce7f26c9
omazoz <oumaymamazoz12@gmail.com>
2023-04-18 00:44:28 +0000

    fixed ?

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,14 +12,14 @@ void print_dog(struct dog *d)
 if (d)
 {
 if (!(d->name))
-printf("Name: (suza)\n");
+printf("Name: (nil)\n");
 else
 printf("Name: %s\n", d->name);
 
 printf("Age: %f\n", d->age);
 
 if (!(d->owner))
-printf("Owner: (oumie)\n");
+printf("Owner: (nil)\n");
 else
 printf("Owner: %s\n", d->owner);
 }
