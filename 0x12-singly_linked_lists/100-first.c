55db9379f26ac87c5d58b4dd587f53cc8ffbdc7e
omazoz <oumaymamazoz12@gmail.com>
2023-04-27 21:53:19 +0000

    fuix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * bmain - function executed before main
+ * beforemain - function executed before main
  * Return: no return.
  */
 
-void __attribute__ ((constructor)) bmain()
+void __attribute__ ((constructor)) beforemain()
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
